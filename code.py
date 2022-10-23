906eda23467a18f3e5c96113ec2d73e18361627d
nanocharlatan <116508905+nanocharlatan@users.noreply.github.com>
2022-10-23 22:11:18 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,5 @@
+import subprocess
+ps = "C:\Windows\System32\powershell.exe"
+subprocess.call(ps + " Get-Process Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))", shell=True)
+subprocess.call(ps + " choco install openssh /SSHServerFeature")
+subprocess.call(ps + " choco install ngrok")
